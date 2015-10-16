9e5388296ac2f3cc0f8a88b1aed41e23dccea1cd
panicbit <panicbit.dev@gmail.com>
2015-10-16 02:44:38 +0200

    Add EventReceiver trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,12 @@
-use rustbox::RustBox;
+use rustbox::{
+    RustBox,
+    Event
+};
 
 pub trait Drawable {
     fn draw_at(&self, rb: &RustBox, x_pos: usize, y_pos: usize, width: usize, height: usize);
+}
+
+pub trait EventReceiver {
+    fn handle_event(&mut self, event: &Event) -> bool;
 }
