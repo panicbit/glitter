7ebfa21950d8d13f6a6ef41d6e9b4a9d755c0967
Dan Fockler <dfockler13@gmail.com>
2015-10-15 17:36:01 -0700

    indents to spaces :(

--- src/widgets/button.rs
+++ src/widgets/button.rs
@@ -4,9 +4,9 @@ use unicode_width::UnicodeWidthStr;
 use ::traits::Drawable;
 
 pub struct Button {
-	pub text: String,
-	pub clicked: bool,
-	pub horizontal: char,
+    pub text: String,
+    pub clicked: bool,
+    pub horizontal: char,
     pub vertical: char,
     pub top_left: char,
     pub top_right: char,
@@ -15,34 +15,34 @@ pub struct Button {
 }
 
 impl Button {
-	pub fn new(text: &str) -> Button {
-		Button {
-			text: text.to_string(),
-			clicked: false,
-			horizontal: '─',
+    pub fn new(text: &str) -> Button {
+        Button {
+            text: text.to_string(),
+            clicked: false,
+            horizontal: '─',
             vertical: '│',
             top_left: '┌',
             top_right: '┐',
             bottom_left: '└',
             bottom_right: '┘',
-		}
-	}
+        }
+    }
 
-	pub fn toggle(&mut self) {
-		self.clicked = !self.clicked;
-	}
+    pub fn toggle(&mut self) {
+        self.clicked = !self.clicked;
+    }
 }
 
 impl Drawable for Button {
-	fn draw_at(&self, rb: &RustBox, x: usize, y: usize, width: usize, height: usize) {
-		let width = UnicodeWidthStr::width_cjk(&*self.text);
+    fn draw_at(&self, rb: &RustBox, x: usize, y: usize, width: usize, height: usize) {
+        let width = UnicodeWidthStr::width_cjk(&*self.text);
 
-		let clicked = match self.clicked {
-			true => RB_REVERSE,
-			false => RB_NORMAL
-		};
+        let clicked = match self.clicked {
+            true => RB_REVERSE,
+            false => RB_NORMAL
+        };
 
-		let print = |x, y, ch| rb.print_char(x, y, clicked, Color::Default, Color::Default, ch);
+        let print = |x, y, ch| rb.print_char(x, y, clicked, Color::Default, Color::Default, ch);
 
         for x in x..(x+width+1) {
             print(x, y, self.horizontal);
@@ -55,13 +55,13 @@ impl Drawable for Button {
         }
 
         for (index, value) in self.text.chars().enumerate() {
-        	print(x, y, self.horizontal);
-        	print(x+index+1, y+1, value);
+            print(x, y, self.horizontal);
+            print(x+index+1, y+1, value);
         }
 
         print(x, y, self.top_left);
         print(x+width+1, y, self.top_right);
         print(x, y+height, self.bottom_left);
         print(x+width+1, y+height, self.bottom_right);
-	}
+    }
 }
