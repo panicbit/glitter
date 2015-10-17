87a5fac83c052cd07e0bad9deada6d981dfb9e01
panicbit <panicbit.dev@gmail.com>
2015-10-17 03:00:11 +0200

    Enhance Frame

--- src/widgets/frame.rs
+++ src/widgets/frame.rs
@@ -1,11 +1,17 @@
 use rustbox::{
     RustBox,
     Color,
-    RB_NORMAL
+    RB_NORMAL,
+    Event
 };
-use ::traits::Drawable;
+use ::traits::{
+    Drawable,
+    EventReceiver,
+    Widget
+};
 
 pub struct Frame {
+    child: Option<Box<Widget>>,
     horizontal: char,
     vertical: char,
     top_left: char,
@@ -15,8 +21,14 @@ pub struct Frame {
 }
 
 impl Frame {
+    pub fn rect_with<W: Widget + 'static>(child: W) -> Frame {
+        let mut frame = Frame::rect();
+        frame.add(child);
+        frame
+    }
     pub fn rect() -> Frame {
         Frame {
+            child: None,
             horizontal: '─',
             vertical: '│',
             top_left: '┌',
@@ -25,8 +37,14 @@ impl Frame {
             bottom_right: '┘'
         }
     }
+    pub fn rounded_with<W: Widget + 'static>(child: W) -> Frame {
+        let mut frame = Frame::rounded();
+        frame.add(child);
+        frame
+    }
     pub fn rounded() -> Frame {
         Frame {
+            child: None,
             horizontal: '─',
             vertical: '│',
             top_left: '╭',
@@ -34,6 +52,9 @@ impl Frame {
             bottom_left: '╰',
             bottom_right: '╯'
         }
+    }
+    pub fn add<W: Widget + 'static>(&mut self, child: W) {
+        self.child = Some(Box::new(child))
     }
 }
 
@@ -60,6 +81,19 @@ impl Drawable for Frame {
         print(x+w, y+h, self.bottom_right);
 
         print(x+w+1, y+h+1, shadow);
+
+        // Render child
+        if let Some(ref child) = self.child {
+            // Don't render child if too frame small
+            let border_size = 3;
+            if w < border_size || h < border_size { return }
+
+            let w = w - border_size;
+            let h = h - border_size;
+
+            child.draw_at(&rb, x + 1, y + 1, w, h);
+        }
+
     }
 
     fn width(&self) -> usize {
@@ -70,5 +104,15 @@ impl Drawable for Frame {
     fn height(&self) -> usize {
         // TODO: Have struct contain width
         5
+    }
+}
+
+impl EventReceiver for Frame {
+    fn handle_event(&mut self, event: &Event) -> bool {
+        if let Some(ref mut child) = self.child {
+            child.handle_event(event)
+        } else {
+            false
+        }
     }
 }
