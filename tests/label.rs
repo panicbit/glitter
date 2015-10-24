1cb9b8c3ed7784e27767764dee753375005f0e31
panicbit <panicbit.dev@gmail.com>
2015-10-24 03:28:28 +0200

    Add frame to label cutoff test

--- tests/label.rs
+++ tests/label.rs
@@ -1,18 +1,20 @@
 extern crate glitter;
 mod common;
 use common::*;
-use glitter::widgets::Label;
+use glitter::widgets::{Label,Frame};
 use glitter::traits::*;
 
 #[test]
 #[ignore]
 /// # Expected
-/// The text "Success!" is displayed at (1,1)
+/// The text "Success!" is displayed inside a frame
 fn tui_label_cutoff() {
     simple_test(|rb| {
         let mut label = Label::new(());
         label.set_text("Success!Failure!");
-        label.draw_at(rb, 1, 1, 8, 1);
+        let mut frame = Frame::new(());
+        frame.add(label);
+        frame.draw_at(rb, 0, 0, 11, 4);
         rb.present();
     })
 }
